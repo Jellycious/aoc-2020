b275b76474243240b0377fc45ee4597c22353dd8
Jelle Meijer <jellejanwil@gmail.com>
2020-12-02 10:15:19 +0100

    day 2

--- 1/expense.hs
+++ 1/expense.hs
@@ -1,8 +1,8 @@
 import System.IO
 -- First Challenge
-f2020 xs = [x * y | x <- xs, y <- xs, x + y == 2020] 
+f2020 xs = [x * y | x <- xs, y <- xs, x + y == 2020]
 -- Second Challenge
-g2020 xs = [x * y * z | x <- xs, y <- xs, z <- xs, x + y + z == 2020] 
+g2020 xs = [x * y * z | x <- xs, y <- xs, z <- xs, x + y + z == 2020]
 
 main = do
     content <- (fmap read <$> lines <$> readFile ("input.txt")) :: IO [Int]
---
+++ 2/challenge.hs
@@ -0,0 +1,31 @@
+import System.IO
+
+count c ([]) = 0 
+count c (x:xs) = if x==c then 1 + count c xs else count c xs 
+
+splitBy :: Char -> String -> (String, String)
+splitBy c str = splitBy' c str []
+    where splitBy' c [] ys      = (ys, []) 
+          splitBy' c (x:xs) ys  = if x == c then (ys, xs) else splitBy' c xs (ys++[x])
+
+getL (l,_,_,_) = l
+getH (_,h,_,_) = h
+getC (_,_,c,_) = c
+getW (_,_,_,w) = w
+
+
+parse s = (low, high, character!!0, tail word)
+    where (range, rest) = splitBy ' ' s
+          (lows, highs) = splitBy '-' range
+          (low, high) = (read lows :: Int, read highs :: Int)
+          (character, word) = splitBy ':' rest
+
+validPass :: String -> Bool
+validPass s = cnt `elem` [low..high]
+    where (low, high, c, w) = parse s
+          cnt = count c w
+
+main = do
+    content <- (lines <$> readFile ("input.txt")) :: IO [String]
+    correct <- pure $ count True (validPass <$> content)
+    return correct
