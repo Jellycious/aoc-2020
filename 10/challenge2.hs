706a75c5ea54511ffdbfd4d6f7f8023a7708b3e1
Jelle Meijer <jellejanwil@gmail.com>
2020-12-10 13:16:27 +0100

    minor change 2

--- 10/challenge2.hs
+++ 10/challenge2.hs
@@ -14,13 +14,14 @@ solver m (x:xs) ls = solver m' xs (x:ls)
           arr   = sum $ map (fromJust . flip M.lookup m) cands
           m'    = M.insert x arr m
 
-solve :: [Int] -> Int -> M.Map Int Int
-solve nums end = solver (M.singleton end 1) (reverse (0:nums)) [end]
+solve :: [Int] -> M.Map Int Int
+solve nums = solver (M.singleton end 1) (reverse (0:nums)) [end]
+    where end = maximum nums + 3
 
 main :: IO Int
 main = do
     input <- lines <$> readFile "input.txt"
     nums <- return $ sort $ map read input :: IO [Int] 
-    sol <- return $ solve nums $ maximum nums + 3
+    sol <- return $ solve nums
     return $ sol M.! 0
     
